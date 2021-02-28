470a4109d33fdaf2f9ba7b2dcbc3d3840906d242
Ian Jairo <ijtg.ian@gmail.com>
2021-02-28 08:59:59 -0300

    Front: Casa with Toggles

--- SmartHomeApp/src/app/casa/casa.page.ts
+++ SmartHomeApp/src/app/casa/casa.page.ts
@@ -6,8 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./casa.page.scss'],
 })
 export class CasaPage implements OnInit {
-
+  public isDisabledReorder: boolean = true
   constructor() { }
+
+  reorderGroup(isDisabledReorder) {
+    if (isDisabledReorder) {
+      return this.isDisabledReorder = false
+    } else {
+      return this.isDisabledReorder = true
+    }
+  }
+
+  homeOptions(event, type) {
+    if (type == "pool") {
+      if (event.detail.checked) {
+
+      } else {
+
+      }
+
+    } else if (type == "livingRoom") {
+      if (event.detail.checked) {
+
+      } else {
+
+      }
+    } else {
+      if (event.detail.checked) {
+        
+      } else {
+
+      }
+    }
+
+
+  }
 
   ngOnInit() {
   }
